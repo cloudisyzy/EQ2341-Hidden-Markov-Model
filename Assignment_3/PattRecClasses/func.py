850353e4e14fcf104be2edc56426b3cce4861db2
cloudisyzy <yangzy0516@outlook.com>
2024-05-11 20:36:42 +0200

    update

--- Assignment_3/PattRecClasses/func.py
+++ Assignment_3/PattRecClasses/func.py
@@ -6,7 +6,7 @@ from collections import Counter
 
 
 def load_data(path, averaging=True, window=2):
-
+    ## load data from .csv file, moving average is conducted if averaging=True, MA order = window, default=2
     data = np.genfromtxt(path, delimiter=',', names=True)
     x = data['x']
     y = data['y']
@@ -27,7 +27,7 @@ def load_data(path, averaging=True, window=2):
     
     
 def plot_fft(data, sample_rate):
-    
+    ## to observe how moving average affect the high frequency components 
     n, T = data.shape
     freq = np.fft.fftfreq(T, d=1/sample_rate)
     # Only show one (x) axis of data
@@ -69,7 +69,147 @@ def compute_pX(observations, dists, scale=True):
         return scaled_pX
     else:
         return unscaled_pX
+
+
+def BaumWelch(q, A, dist, data):
+    ## train HMM using Baum-Welch algorithm, equivalent to EM, data <=> observed seq
+    ##                                dist <=> source distributions
+    ##                                A <=> transition prob
+    ##                                q <=> initial prob
     
+    # Initialization
+    mc = MarkovChain(q, A)
+    pX = compute_pX(data, dist, scale=True)
+    n_states = q.shape[0] # # of hidden states
+    T = data.shape[1] # length of obersavations
+    
+    # Forward & Backward
+    alpha_hat, c = mc.forward(pX)
+    beta_hat = mc.backward(pX, c)
+    # print(alpha_hat)
+    # print(c)
+    # print(beta_hat)
+    
+    # Compute Gamma
+    gamma = np.zeros_like(alpha_hat) 
+    for j in range(gamma.shape[0]):
+        for t in range(gamma.shape[1]):
+            gamma[j,t] = alpha_hat[j,t] * beta_hat[j,t] * c[t] # 5.63 
+    # print(gamma)
+            
+    # Update q
+    q_updated = gamma[:,0] / np.sum(gamma[:,0]) # 7.54 
+    # print(q_updated)
+    
+    # Update A
+    eps = np.zeros((n_states, n_states, T))
+    eps_hat = np.zeros((n_states, n_states))
+    for i in range(n_states):
+        for j in range(n_states):
+            for t in range(T-1):
+                eps[i,j,t] = alpha_hat[i,t] * A[i,j] * pX[j,t+1] * beta_hat[j,t+1] # 6.19
+            eps_hat[i,j] = np.sum(eps[i,j,:]) # 6.12
+    A_updated = np.zeros_like(A)      
+    denominator_A = np.sum(eps_hat, axis=1)
+    for i in range(eps_hat.shape[0]):
+        A_updated[i,:] = eps_hat[i,:] / denominator_A[i] # 6.13
+    # print(A_updated)
+        
+    # Update B, equivalent to update the parameters of source distribution of each state
+    ## Update Means
+    mu_updated = np.zeros((n_states, dist[0].means.shape[0]))
+    for i in range(n_states):
+        temp = np.zeros_like(data)
+        for t in range(T):
+            temp[:,t] = gamma[i,t] * data[:,t]
+        numerator_mu = np.sum(temp, axis=1)
+        denominator_mu = np.sum(gamma[i,:], axis=0)
+        mu_updated[i] = numerator_mu / denominator_mu # 7.70 
+    # print(mu_updated)
+    ## Update Covariances
+    cov_updated = np.zeros((n_states, dist[0].cov.shape[0], dist[0].cov.shape[1]))
+    for i in range(n_states):
+        temp = np.zeros((data.shape[0], data.shape[0], T)) # data.shape[0] is equivalent to dist[0].cov.shape[0]
+        for t in range(T):
+            temp[:,:,t] = gamma[i,t] * ( (data[:,t]-mu_updated[i,:]).reshape(n_states, -1) * (data[:,t]-mu_updated[i,:]) )
+        numerator_cov = np.sum(temp, axis=2)
+        denominator_cov = np.sum(gamma[i,:], axis=0)
+        cov_updated[i,:,:] = numerator_cov / denominator_cov # 7.70 
+    # print(cov_updated)
+    ## Update General Distributions
+    dist_updated = []
+    for i, state_dist in enumerate(dist):
+        state_dist.means = mu_updated[i]
+        state_dist.cov = cov_updated[i]
+        dist_updated.append(state_dist)
+    
+    return q_updated, A_updated, dist_updated
+
+
+def HMM_prediction(q, A, dist, data, decision=False):
+    ## use HMM to predict the status of a given observation (data)
+    ## decision=False -> return only predicted state seq (array consists of 1, 2, 3), decided by the argmax of the conditional prob of states given observations (gamma)
+    ## decision=True -> return the hard decision of class (base on # of 1s, 2s, 3s) and state seq
+    
+    # Initialization
+    prefix = np.load('PattRecClasses/prefix.npy')
+    data = np.concatenate((prefix, data), axis=1)
+    mc = MarkovChain(q, A)
+    pX = compute_pX(data, dist, scale=True)
+    
+    # Forward & Backward
+    alpha_hat, c = mc.forward(pX)
+    beta_hat = mc.backward(pX, c)
+    
+    # Compute gamma, P of state # given observations
+    gamma = np.zeros_like(alpha_hat) 
+    for j in range(gamma.shape[0]):
+        for t in range(gamma.shape[1]):
+            gamma[j,t] = alpha_hat[j,t] * beta_hat[j,t] * c[t]
+            
+    pred_state_seq = np.argmax(gamma, axis=0) + 1
+    
+    if decision == False:
+        return pred_state_seq
+    else:
+        count_state_freq = Counter(pred_state_seq)
+        pred_class = count_state_freq.most_common(1)[0][0]
+        return pred_state_seq, pred_class
+
+
+def plot_prediction_HMM(state_seq, observations, size=(10, 6), ppi=120, title='Prediction of HMM Given Observations', save_path=None):
+    ## plot the observation seq along with the predicted states in a single plot with two scales
+    
+    plt.figure(figsize=size, dpi=ppi)
+    
+    ax1 = plt.gca()
+    ax1.plot(observations[0], 'g', label='X-axis')
+    ax1.plot(observations[1], 'b', label='Y-axis')
+    ax1.plot(observations[2], 'grey', label='Z-axis')
+    ax1.set_ylim([-15, 15])
+    ax1.set_ylabel('Amplitude of Accelerometer Measurements')
+    ax1.yaxis.label.set_color('blue')
+    ax1.tick_params(axis='y', colors='blue')
+    ax1.legend(loc='upper left')
+
+    ax2 = ax1.twinx()
+    ax2.plot(state_seq, 'r.', label='Predicted State')
+    ax2.set_ylim([0.5, 3.5])
+    ax2.set_ylabel('Predicted State Number')
+    ax2.yaxis.label.set_color('red')
+    ax2.tick_params(axis='y', colors='red')
+    ax2.set_yticks([1, 2, 3])
+    ax2.legend(loc='upper right') 
+
+    ax1.set_xlabel('Time')
+    plt.title(title)
+    plt.grid()
+    
+    if save_path:
+        plt.savefig(save_path, transparent=False, facecolor='w')
+
+    plt.show()
+
     
 # def BaumWelch(q, A, dist, data):
     
@@ -137,138 +277,12 @@ def compute_pX(observations, dists, scale=True):
 #     return q_updated, A_updated, mu_updated, cov_updated
 
 
-def BaumWelch(q, A, dist, data):
-    
-    # Initialization
-    mc = MarkovChain(q, A)
-    pX = compute_pX(data, dist, scale=True)
-    n_states = q.shape[0] # # of hidden states
-    T = data.shape[1] # length of obersavations
-    
-    # Forward & Backward
-    alpha_hat, c = mc.forward(pX)
-    beta_hat = mc.backward(pX, c)
-    # print(alpha_hat)
-    # print(c)
-    # print(beta_hat)
-    
-    # Compute Gamma
-    gamma = np.zeros_like(alpha_hat) 
-    for j in range(gamma.shape[0]):
-        for t in range(gamma.shape[1]):
-            gamma[j,t] = alpha_hat[j,t] * beta_hat[j,t] * c[t] # 5.63 !!! gamma 和参考的算的不一样，怀疑是参考错误，尤其在c[t]的使用上,如果将参考中c[t]改为c[j],则结果相同，不过为转置
-    # print(gamma)
-            
-    # Update q
-    q_updated = gamma[:,0] / np.sum(gamma[:,0]) # 7.54 !!! gamma 和参考的算的不一样，q肯定不一样，如果将参考中c[t]改为c[j],则结果相同
-    # print(q_updated)
-    
-    # Update A
-    eps = np.zeros((n_states, n_states, T))
-    eps_hat = np.zeros((n_states, n_states))
-    for i in range(n_states):
-        for j in range(n_states):
-            for t in range(T-1):
-                eps[i,j,t] = alpha_hat[i,t] * A[i,j] * pX[j,t+1] * beta_hat[j,t+1] # 6.19
-            eps_hat[i,j] = np.sum(eps[i,j,:]) # 6.12
-    A_updated = np.zeros_like(A)      
-    denominator_A = np.sum(eps_hat, axis=1)
-    for i in range(eps_hat.shape[0]):
-        A_updated[i,:] = eps_hat[i,:] / denominator_A[i] # 6.13
-    # print(A_updated)
-        
-    # Update B, equivalent to update the parameters of source distribution of each state
-    ## Update Means
-    mu_updated = np.zeros((n_states, dist[0].means.shape[0]))
-    for i in range(n_states):
-        temp = np.zeros_like(data)
-        for t in range(T):
-            temp[:,t] = gamma[i,t] * data[:,t]
-        numerator_mu = np.sum(temp, axis=1)
-        denominator_mu = np.sum(gamma[i,:], axis=0)
-        mu_updated[i] = numerator_mu / denominator_mu # 7.70 ！！！gamma 和参考的算的不一样，mu肯定不一样，如果将参考中c[t]改为c[j],则结果相同
-    # print(mu_updated)
-    ## Update Covariances
-    cov_updated = np.zeros((n_states, dist[0].cov.shape[0], dist[0].cov.shape[1]))
-    for i in range(n_states):
-        temp = np.zeros((data.shape[0], data.shape[0], T)) # data.shape[0] is equivalent to dist[0].cov.shape[0]
-        for t in range(T):
-            temp[:,:,t] = gamma[i,t] * ( (data[:,t]-mu_updated[i,:]).reshape(n_states, -1) * (data[:,t]-mu_updated[i,:]) )
-        numerator_cov = np.sum(temp, axis=2)
-        denominator_cov = np.sum(gamma[i,:], axis=0)
-        cov_updated[i,:,:] = numerator_cov / denominator_cov # 7.70 ！！！gamma 和参考的算的不一样，cov肯定不一样，如果将参考中c[t]改为c[j],则结果相同
-    # print(cov_updated)
-    ## Update General Distributions
-    dist_updated = []
-    for i, state_dist in enumerate(dist):
-        state_dist.means = mu_updated[i]
-        state_dist.cov = cov_updated[i]
-        dist_updated.append(state_dist)
-    
-    return q_updated, A_updated, dist_updated
 
-
-def dist_update(old_dist, new_mu, new_cov):
-    new_dist = []
-    for i, state_dist in enumerate(old_dist):
-        state_dist.means = new_mu[i]
-        state_dist.cov = new_cov[i]
-        new_dist.append(state_dist)
+# def dist_update(old_dist, new_mu, new_cov):
+#     new_dist = []
+#     for i, state_dist in enumerate(old_dist):
+#         state_dist.means = new_mu[i]
+#         state_dist.cov = new_cov[i]
+#         new_dist.append(state_dist)
     
-    return new_dist
-
-
-def HMM_prediction(q, A, dist, data, decision=False):
-    
-    # Initialization
-    mc = MarkovChain(q, A)
-    pX = compute_pX(data, dist, scale=True)
-    
-    # Forward & Backward
-    alpha_hat, c = mc.forward(pX)
-    beta_hat = mc.backward(pX, c)
-    
-    # Compute gamma, P of state # given observations
-    gamma = np.zeros_like(alpha_hat) 
-    for j in range(gamma.shape[0]):
-        for t in range(gamma.shape[1]):
-            gamma[j,t] = alpha_hat[j,t] * beta_hat[j,t] * c[t]
-            
-    pred_state_seq = np.argmax(gamma, axis=0) + 1
-    
-    if decision == False:
-        return pred_state_seq
-    else:
-        count_state_freq = Counter(pred_state_seq)
-        pred_class = count_state_freq.most_common(1)[0][0]
-        return pred_state_seq, pred_class
-
-
-def plot_prediction_HMM(state_seq, observations):
-    plt.figure(figsize=(10, 6), dpi=120)
-    
-    ax1 = plt.gca()
-    ax1.plot(observations[0], 'g', label='X-axis')
-    ax1.plot(observations[1], 'b', label='Y-axis')
-    ax1.plot(observations[2], 'grey', label='Z-axis')
-    ax1.set_ylim([-15, 15])
-    ax1.set_ylabel('Amplitude of Accelerometer Measurements')
-    ax1.yaxis.label.set_color('blue')
-    ax1.tick_params(axis='y', colors='blue')
-    ax1.legend(loc='upper left')
-
-    ax2 = ax1.twinx()
-    ax2.plot(state_seq, 'r.', label='Predicted State')
-    ax2.set_ylim([0.5, 3.5])
-    ax2.set_ylabel('Predicted State Number')
-    ax2.yaxis.label.set_color('red')
-    ax2.tick_params(axis='y', colors='red')
-    ax2.set_yticks([1, 2, 3])
-    ax2.legend(loc='upper right') 
-
-    ax1.set_xlabel('Time')
-    plt.title('Prediction of HMM Given Observations')
-    plt.grid()
-    plt.show()
-
-
+#     return new_dist
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# EQ2341-Hidden-Markov-Model
+# Hidden Markov Model in Human Activity (Standing, Walking, Running) Classification
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Usage and Structure](#usage-and-structure)
+- [Repository Structure](#repository-structure)
+
+
+## Introduction
+
+This repository hosts the code for the project of Pattern Recognition and Machine Learning course. We use HMM to classify the accelerometer data sequence (3 axis) recorded from three different human activities (Standing, Walking, Running). The best approach achieves **98.89% ** classification accuracy in validation data.
+
+
+## Installation
+
+To set up the project environment, clone this repository and install the required dependencies:
+
+```bash
+git clone https://github.com/cloudisyzy/EQ2341-Hidden-Markov-Model.git
+pip install -r requirements.txt
+```
+
+## Usage and Structure
+
+This is a small project so I primarily use Jupyter Notebooks (.ipynb) to conduct simulations. If you are unfarmiliar with this, you can also convert the notebooks to .py files using
+```bash
+jupyter nbconvert --to script *.ipynb
+```
+But I cannot guarantee all scripts can be executed with no error in .py format
+
+`train.ipynb`: Train HMM using training data and EM, some key features of trained HMM are displayed.
+`test_classification.ipynb`: Use the trained HMM to classify short samples from the three classes. The short samples are not involved in training. Plot the classification accuracy and confusion matrix
+`test_state_prediction.ipynb`: Use the trained HMM to predict the state sequence of two long samples, which are also not involved in training. Plot the predicted state sequences.
+`visualize_data.ipynb`: Display the accelerometer data in 2D and 3D fashion. Readers can ignore this script if error arises, possibly due to javascript configurations.
+
+For all .ipynb files, you don't need to change any variables, just click RUN.
+
+`data`: Data collected by the authors are stored in this folder. `train.csv` is used to train HMM. `test_1.csv` and `test_2.csv` are used in validating the accuracy of predicted state of long sequences. 
+	`data/running_test`
